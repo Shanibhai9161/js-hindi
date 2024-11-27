b1f72595e4fee9677e8f4d1be2ee529c2d4d67e6
Shanibhai9161 <shaniyauvanshi7617@gmail.com>
2024-11-27 09:17:16 +0530

    first commit

---
+++ 1_basics/datatype-summary.js
@@ -0,0 +1,26 @@
+//  Primitive 
+
+// 7 types : string, number, boolean,  null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id ===anotherId);
+
+const bigNumber = 34567885567n
+
+
+
+
+
+
+// Reference {Non Primitive}
+
+// Array, objects, functions 
+
